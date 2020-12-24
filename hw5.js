2be6bd0cd2d1781363d7c0edc0fb616973d3519c
ptrnkOstap <ptrnkostap@gmail.com>
2020-12-25 02:19:11 +0300

    hw5

--- hw5.js
+++ hw5.js
@@ -110,5 +110,57 @@ const chessBoard = {
 //chessBoard.renderChessBoard();
 //Task3
 {
+    {
+        const basket = {                    //корзина с массивом объектов и методом подсчета стоимости
+            products: Array(),
+            countBasketPrice() {
+                if (this.products.length === 0) return null;
+                return this.products.reduce((total, curItem) => (total + curItem.price * curItem.quantity), 0);
+            },
+            countBasketProducts() {
+                if (this.products.length === 0) return null;
+                return this.products.reduce(((total, currItem) => total + currItem.quantity), 0);
+            },
+            addProduct(pr) {
+                const prodIndex = this.products.findIndex(p => p.article === pr.article);
+                if (prodIndex !== -1) {
+                    this.products[prodIndex].quantity += pr.quantity;
+                } else {
+                    basket.products.push(pr);
+                }
+            },
+            removeProduct(prodArticle) {
+                if (this.products.length === 0) return null;
+                const prodIndex = this.products.findIndex(p => p.article === prodArticle);
+                if (prodIndex !== -1) {
+                    this.products.splice(prodIndex, 1);
+                }
+            },
+            renderBasket(){
+                const basketHTML=document.getElementById('shopping-cart-overview');
+                if(this.products.length===0) {
+                    basketHTML.insertAdjacentElement("beforebegin", document.createElement('div',{}).innerHTML='Корзина пуста')
+                }
+            }
+        }
+
+        const Product = {                  // сущность 1 продукта
+            article: null,
+            title: null,
+            price: null,
+            quantity: null
+        }
+
+        basket.addProduct({article: 333444, title: 'T-shirt', price: 10, quantity: 2});  //добавляем пару товаров
+        basket.addProduct({article: 123854, title: 'Shorts', price: 15, quantity: 1});   //в виде объектов в корзину
+
+        basket.addProduct({article: 123854, title: 'Shorts', price: 15, quantity: 1});   //чекаем addProduct,добавив +1
+        // уже существующего артикула
+
+        basket.removeProduct(123854);   //пробуем удалить товар из корзины
+
+        // выводим стоимость (20) и количество (2) товаров
+        console.log(`В вашей корзине ${basket.countBasketProducts()} товара(ов) на сумму ${basket.countBasketPrice()}`);
+    }
 
 }
